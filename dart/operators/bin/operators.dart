84e65cf2825b67cbff0db790f861e028998cb3fa
MOLDOGAZY KABYLBEKOV <moldogazy.kabylbekov.kk@gmail.com>
2023-03-17 15:00:55 +0600

    Updated flutter hws folder

--- dart/operators/bin/operators.dart
+++ dart/operators/bin/operators.dart
@@ -1,18 +1,32 @@
-// import 'package:operators/operators.dart' as operators;
+import 'dart:math';
 
 void main(List<String> arguments) {
-  print('This\'s adder: $adder(12, 44)');
+  print('------------- Adder -------------');
   final addOnceMore = 45 + adder(4334, 22);
   print('Thi\'s one more time adder: $addOnceMore');
 
+  print('------------- Substraction -------------');
   final substract = addOnceMore - 2323;
   print('This\'s substraction: $substract');
-
-  print('----------------');
-
   print(substracter(12, 21));
 
+  print('------------- Multiplication -------------');
   print(milti(12, 21));
+
+  print('------------- Divider -------------');
+  print(divider(12, 222));
+
+  print('------------- Divider Intager -------------');
+  print(divInt(12, 222));
+
+  print('------------- Remainder -------------');
+  print(remainder(12, 222));
+
+  print('------------- List -------------');
+  print(a[1]);
+
+  print('------------- Random -------------');
+  print('intValue: $intValue, nextDouble: $nextDouble, nextBool: $nextBool');
 }
 
 // Functions
@@ -35,5 +49,22 @@ num divider(num a, num b) {
   final result = a / b;
   return result;
 }
+
+num divInt(num a, int b) {
+  final result = a ~/ b;
+  return result;
+}
+
+num remainder(int a, int b) {
+  final result = a % b;
+  return result;
+}
+
+List a = <String>['a', 'b', 'c', 'd'];
+
+var intValue = Random().nextInt(100);
+var nextDouble = Random().nextDouble();
+var nextBool = Random().nextBool();
+
 
 // https://youtu.be/i7aaXLb5Qpo?list=PLWu9OGm_n5CcahNAV5AFUgww4Ee_5tJgU&t=1259
