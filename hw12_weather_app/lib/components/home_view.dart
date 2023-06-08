9eb46265cc6b46646cf07edfca81eeb76ce3a96c
MOLDOGAZY KABYLBEKOV <moldogazy.kabylbekov.kk@gmail.com>
2023-06-08 20:19:38 +0600

    Weather App's UI's ready to use

--- hw12_weather_app/lib/components/home_view.dart
+++ hw12_weather_app/lib/components/home_view.dart
@@ -24,8 +24,58 @@ class _MyHomeViewState extends State<MyHomeView> {
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
+            fit: BoxFit.cover,
+            opacity: 0.4,
             image: AssetImage('./assets/bg.jpg'),
           ),
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Icon(
+                    Icons.near_me_rounded,
+                    color: AppColors.white,
+                    size: 40,
+                  ),
+                  Icon(
+                    Icons.location_city_outlined,
+                    color: AppColors.white,
+                    size: 40,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 50),
+            Row(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(left: 10),
+                ),
+                const Text('8', style: AppTextStyles.weaherDegree),
+                Image.asset('./assets/cloud.png', height: 150)
+              ],
+            ),
+            const SizedBox(height: 50),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Text(
+                'You\'ll need and'.replaceAll(' ', '\n'),
+                style: AppTextStyles.description,
+              ),
+            ),
+            const SizedBox(height: 70),
+            const Align(
+              alignment: Alignment.centerRight,
+              child: Text(
+                'Bishkek',
+                style: AppTextStyles.description,
+              ),
+            )
+          ],
         ),
       ),
     );
--- hw12_weather_app/lib/constants/text_styles.dart
+++ hw12_weather_app/lib/constants/text_styles.dart
@@ -7,4 +7,15 @@ class AppTextStyles {
     fontWeight: FontWeight.w800,
     color: AppColors.black,
   );
+
+  static const weaherDegree = TextStyle(
+    color: AppColors.white,
+    fontSize: 100,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const description = TextStyle(
+    color: AppColors.white,
+    fontSize: 70,
+  );
 }
