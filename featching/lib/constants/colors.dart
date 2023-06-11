66055a60ab996c998b23b272fa2c063fd8cf2f1a
MOLDOGAZY KABYLBEKOV <moldogazy.kabylbekov.kk@gmail.com>
2023-06-11 12:50:12 +0600

    Featching data is done

---
+++ featching/lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const appBg = Color(0xfff4f3f3);
+  static const white = Color(0xffffffff);
+  static const black = Color(0xff000000);
+}
---
+++ featching/lib/constants/text_styles.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+class AppTextStyles {
+  static const appTitle = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w800,
+    color: AppColors.black,
+  );
+
+  static const weaherDegree = TextStyle(
+    color: AppColors.white,
+    fontSize: 100,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const description = TextStyle(
+    color: AppColors.white,
+    fontSize: 70,
+  );
+}
