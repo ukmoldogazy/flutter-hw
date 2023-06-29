668eec257b132400d8082b9f9354ac022d0aba84
MOLDOGAZY KABYLBEKOV <moldogazy.kabylbekov.kk@gmail.com>
2023-06-30 01:06:36 +0600

    hw15_firebase is going on

--- hw15_firebase/lib/components/todo.dart
+++ hw15_firebase/lib/components/todo.dart
@@ -9,6 +9,11 @@ class Todo extends StatefulWidget {
 
 class _TodoState extends State<Todo> {
   bool isCompleted = false;
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _authorController = TextEditingController();
+  final _formKey = GlobalKey<State>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,41 +22,57 @@ class _TodoState extends State<Todo> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            TextFormField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Title',
-                filled: true,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _titleController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Title',
+                  filled: true,
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            TextFormField(
-              maxLines: 10,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Description',
-                filled: true,
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: _descriptionController,
+                maxLines: 10,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Description',
+                  filled: true,
+                ),
               ),
-            ),
-            CheckboxListTile(
-              value: isCompleted,
-              onChanged: (value) {
-                setState(() {
-                  isCompleted = value!;
-                });
-              },
-            ),
-            const SizedBox(height: 10),
-            TextFormField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Author',
-                filled: true,
+              CheckboxListTile(
+                value: isCompleted,
+                onChanged: (value) {
+                  setState(() {
+                    isCompleted = value!;
+                  });
+                },
               ),
-            ),
-          ],
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: _authorController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Author',
+                  filled: true,
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+                onPressed: () {},
+                icon: const Icon(Icons.arrow_drop_up, color: Colors.black),
+                label: const Text(
+                  'Send',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
